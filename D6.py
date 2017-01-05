dbcff8c741d629dbc650eb68f373af132cf3ee41
n1cfury <fukugetin@gmail.com>
2017-01-04 23:11:01 -0800

    Fixed indentation and moved global variables to the top. Next I'm going to remove the feature that requires you to enter a number before it prints out a numbered dice.

--- D6.py
+++ D6.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
 #This is my attempt to create a Dice Rolling program to use in absence of having the real thing
-# Added .py extension
 # Print statement fixed from print(" )" to print(" ")
 
+import random                                   #Moved to the top of the program for aesthetics
 endprogram=0
+number=random.randint(1,6)
+
 while endprogram != "q":
-  import random
-  print("Welcome to eDice")
-  print("Press the Enter Key to Roll the D6")
-  input(
-  number=random.randint(1,6)
-  if number == 1:
+
+print("Welcome to eDice")                       #Changed indentation of print statements
+print("Press the Enter Key to Roll the D6")
+input()
+
+  if number == 1:                               #Fixed indentation (syntax) error
       print("[----------]")
       print("[          ]")
       print("[    0     ]")
@@ -42,7 +44,7 @@ while endprogram != "q":
        print("[----------]")
        print("[  0    0  ]")
        print("[          ]")
-       print("[  0    0 ]")
+       print("[  0    0  ]")
        print("[----------]")
        print()
        print("Type 'q' to Quit")
