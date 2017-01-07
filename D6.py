eb9ef5c5fa852c4b0d0615272f7785b95163ab29
n1cfury <fukugetin@gmail.com>
2017-01-06 20:01:28 -0800

    Using the string didnt work formatting wise.  Going to make some adjustments to the functions

--- D6.py
+++ D6.py
@@ -4,96 +4,79 @@
 
 import random                                   #Moved to the top of the program for aesthetics
 endprogram=0
+num=random.randint(1,6)
+runthis= input("Would you like to roll the dice? ")
 
-
-
-'''def banner():
+def banner():
   print("Welcome to eDice")                       #Changed indentation of print statements
   print("Press the Enter Key to Roll the D6")
   print("Press 'q' to quit the program")
-'''
-def diceroll():
-  num=random.randint(1,6)
-  print("[----------]")
-  print("[          ]")
-  print("[    %s    ]") %(num) 
-  print("[          ]")
-  print("[----------]")
-  runthis
+  print("")
 
+def diceroll():  
+  if num  = 1:                               #Fixed indentation (syntax) error
+    print("[----------]")
+    print("[          ]")
+    print("[     1    ]")
+    print("[          ]"
+    print("[----------]")
+    print()
+    Print("Type 'q' to Quit")
+    runthis
 
+  elif num = 2:
+    print("[----------]")
+    print("[          ]")
+    print("[  2   2   ]")
+    print("[          ]")
+    print("[----------]")
+    print()
+    runthis
+
+  elif num = 3:
+    print("[----------]")
+    print("[  3       ]")
+    print("[     3    ]")
+    print("[        3 ]")
+    print("[----------]")
+    print()
+    runthis
+
+  elif num = 4:
+    print("[----------]")
+    print("[  4    4  ]")
+    print("[          ]")
+    print("[  4    4  ]")
+    print("[----------]")
+    print()
+    runthis
+
+  elif num = 5:
+    print("[----------]")
+    print("[  5     5 ]")
+    print("[     5    ]")
+    print("[  5     5 ]")
+    print("[----------]")
+    print()
+    runthis
+
+  elif num = 6:
+    print("[----------]")
+    print("[  6    6  ]")
+    print("[  6    6  ]")
+    print("[  6    6  ]")
+    print("[----------]")
+    print("")
+    runthis
+
+  else:
+    print ("Sorry, wrong input, try again")
+    main()
+ 
 def main():  
-  runthis= input("Would you like to roll the dice? ")
-  while runthis is 'Y' or 'y':
+  banner()
+  while runthis = "Y" or "y":
     diecroll()
 
 if __name__ == "__main__":
   main()
-
-'''
-  if number == 1:                               #Fixed indentation (syntax) error
-      print("[----------]")
-      print("[          ]")
-      print("[     1    ]")
-      print("[          ]")
-      print("[----------]")
-      print()
-      Print("Type 'q' to Quit")
-      endprogram=input()
-  if number==2:
-       print("[----------]")
-       print("[          ]")
-       print("[  2   2   ]")
-       print("[          ]")
-       print("[----------]")
-       print()
-       print("Type 'q' to Quit")
-       endprogram=input()
-  if number==3:
-       print("[----------]")
-       print("[  3       ]")
-       print("[     3    ]")
-       print("[        3 ]")
-       print("[----------]")
-       print()
-       print("Type 'q' to Quit")
-       endprogram=input()
-  if number==4:
-       print("[----------]")
-       print("[  4    4  ]")
-       print("[          ]")
-       print("[  4    4  ]")
-       print("[----------]")
-       print()
-       print("Type 'q' to Quit")
-       endprogram=input()
-  if number==5:
-       print("[----------]")
-       print("[  5     5 ]")
-       print("[     5    ]")
-       print("[  5     5 ]")
-       print("[----------]")
-       print()
-       print("Type 'q' to Quit")
-       endprogram=input()
-  if number==6:
-       print("[----------]")
-       print("[  6    6  ]")
-       print("[  6    6  ]")
-       print("[  6    6  ]")
-       print("[----------]")
-       print()
-       print("Type 'q' to Quit")
-       endprogram=input()   
-'''  
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
